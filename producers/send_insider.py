66b288def8dc292790ad67a8a6acafcc6f815ac2
Jaco Becker <becker_jaco@hotmail.com>
2025-09-04 04:08:45 +0000

    Made changes to insider event simulation script

--- producers/send_insider.py
+++ producers/send_insider.py
@@ -5,11 +5,14 @@ from azure.eventhub import EventHubProducerClient, EventData
 EH_SEND=os.getenv('EH_SEND')
 TOPIC=os.getenv('TOPIC','insider_events')
 
-producer = EventHubProducerClient.from_connection_string(conn_str=EH_SEND, eventhub_name=TOPIC); fk=Faker(); users=[fk.user_name() for _ in range(500)]
+#producer = EventHubProducerClient.from_connection_string(conn_str=EH_SEND, eventhub_name=TOPIC); 
+fk=Faker(); 
+users=[fk.user_name() for _ in range(5000)]
 
 while True:
     u=random.choice(users)
     for t in ['PRIV_ESC','BULK_EXPORT','EXT_SHARE']:
         evt={'type':t,'actor':u,'ts':int(time.time()*1000)}
-        producer.send_batch([EventData(json.dumps(evt).encode())]); time.sleep(0.2)
-    time.sleep(1)
+        #producer.send_batch([EventData(json.dumps(evt).encode())]); time.sleep(0.2)
+        print(json.dumps(evt))
+    time.sleep(random.uniform(0.1,5))
