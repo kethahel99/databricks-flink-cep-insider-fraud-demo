3ab8f08d25a72db76abcf3ce8ba79a8278f1078b
Jaco Becker <becker_jaco@hotmail.com>
2025-09-04 04:04:31 +0000

    Added gitignore

--- producers/send_auth_txn.py
+++ producers/send_auth_txn.py
@@ -8,14 +8,17 @@ from azure.eventhub import EventHubProducerClient, EventData
 EH_SEND=os.getenv('EH_SEND')
 TOPIC=os.getenv('TOPIC','auth_txn')
 
-producer = EventHubProducerClient.from_connection_string(conn_str=EH_SEND, eventhub_name=TOPIC)
+#producer = EventHubProducerClient.from_connection_string(conn_str=EH_SEND, eventhub_name=TOPIC)
 fk = Faker()
 cards=[fk.credit_card_number() for _ in range(100000)]
 while True:
     card=random.choice(cards)
     small = { 'type':'TXN','cardId':card,'amount':round(random.uniform(1,4.5),2),'ts':int(datetime.now().timestamp()*1000)}
-    producer.send_batch([EventData(json.dumps(small).encode())])
-    time.sleep(0.1)
+    print(json.dumps(small))
+    #producer.send_batch([EventData(json.dumps(small).encode())])
+    time.sleep(random.uniform(0.1,5))
     big = { 'type':'TXN','cardId':card,'amount':round(random.uniform(600,2000),2),'ts':int(datetime.now().timestamp()*1000)}
-    producer.send_batch([EventData(json.dumps(big).encode())])
-    time.sleep(1)
+    #producer.send_batch([EventData(json.dumps(big).encode())])
+    # log the transaction to console
+    print(json.dumps(big))
+    time.sleep(random.uniform(0.1,5))
